0308cd2bcd78bf730408b215f6df9f7e8b59effb
boszko <brianboszko04@gmail.com>
2025-08-14 11:24:04 -0300

    kitty mais bonitinho ainda!

--- homerinos/fish.nix
+++ homerinos/fish.nix
@@ -1,5 +1,15 @@
 {pkgs, ...} : {
+home.manager = with pkgs; [
+fishplugins.tide
+];
+
 home-manager.users.boszko = {
   programs.fish.enable = true;
+  plugins = [
+  	{
+        name = "tide";
+        src = pkgs.fishPlugins.tide.src;
+	}
+  ];
 };
 }
