4b2b74e68b88d538d8f030435ca7c7a2766813a3
boszko <brianboszko04@gmail.com>
2025-08-14 11:14:53 -0300

    kitty mais bonitinho

---
+++ homerinos/kitty.nix
@@ -0,0 +1,11 @@
+{
+  programs.kitty = {
+    enable = true;
+    settings = {
+      cursor_trail = 1;
+      cursor_trail_decay = "0.1 0.4";
+      cursor_trail_start_threshold = 0;
+      font_size = 10.5;
+    };
+  };
+}
