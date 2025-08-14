177083d8a4d579a1c25c2ec29e33d6c41435fe3f
boszko <brianboszko04@gmail.com>
2025-08-14 17:19:25 -0300

    bwomp

--- homerinos/fish.nix
+++ homerinos/fish.nix
@@ -1,19 +1,5 @@
-{pkgs, ...} : {
-home.packages = with pkgs; [
-	fishPlugins.tide
-];
-  programs.fish.enable = true;
-
-
-  fish.plugins = [
-  	{
-        name = "tide";
-        src = pkgs.fishPlugins.tide.src;
-	}
-  ];
-
-  shellAlises = {
-  neofetch = "fastfetch";
-
-  };
+{pkgs, ...}: {
+   programs.fish = {
+    enable = true;
+};
 }
