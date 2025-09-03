dd34fe150c0bc021bbdc736f85d38e503045d3fc
boszko <brianboszko04@gmail.com>
2025-09-03 08:36:39 -0300

    .

--- plaptop/configuration.nix
+++ plaptop/configuration.nix
@@ -90,7 +90,6 @@
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
-  };
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
