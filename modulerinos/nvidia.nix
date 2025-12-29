a143e81b8ff3fbe000bc93c957e9efd102728ad6
boszko <brianboszko04@gmail.com>
2025-12-29 19:51:22 -0300

    tweaks para drivers adicionais (problema no baldurs gate)

--- modulerinos/nvidia.nix
+++ modulerinos/nvidia.nix
@@ -1,6 +1,11 @@
-{config, lib, ...}: 
+{config, pkgs, lib, ...}: 
 {
-	hardware.graphics.enable = true;
+	hardware.graphics = {
+		enable = true;
+		extraPackages = with pkgs; [
+			libvdpau-va-gl
+		];
+	};
 	services.xserver.videoDrivers = ["nvidia"];
 	hardware.nvidia = {
 		powerManagement.enable = false;
@@ -8,5 +13,11 @@
 		open = false;
 		nvidiaSettings = true;
 		package = config.boot.kernelPackages.nvidiaPackages.stable;
+	};
+	environment.sessionVariables = {
+	GBM_BACKEND = "nvidia-drm"; # Required for GPU acceleration
+   	LBVA_DRIVER_NAME = "nvidia";
+   	__GLX_VENDOR_LIBRARY_NAME = "nvidia";
+  	VDPAU_DRIVER = "va_gl";
 	};
 }
