aa37e9545e253c1ed59b9516cc2505c0076ba74f
boszko <brianboszko04@gmail.com>
2026-08-27 17:43:26 -0300

    consertar shadowplay nixos

--- modulerinos/shadowplay.nix
+++ modulerinos/shadowplay.nix
@@ -3,10 +3,6 @@
   inputs,
   ...
 }: {
-  imports = [
-    inputs.gsr-ui-nix.nixosModules.default
-  ];
-
   programs.gpu-screen-recorder = {
     package = inputs.gsr-ui-nix.packages.${pkgs.stdenv.hostPlatform.system}.gpu-screen-recorder;
     enable = true;
